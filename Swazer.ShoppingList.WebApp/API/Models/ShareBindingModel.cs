1a2e59a226807acbb55455c5d23575cc66beb1e8
Furkan Vela <armagan_bursa@hotmail.com>
2019-02-19 09:33:48 +0300

    - Some modifications

--- Swazer.ShoppingList.WebApp/API/Models/ShareBindingModel.cs
+++ Swazer.ShoppingList.WebApp/API/Models/ShareBindingModel.cs
@@ -9,7 +9,7 @@ namespace Swazer.ShoppingList.WebApp.API.Models
     public class ShareBindingModel
     {
         public int CartId { get; set; }
-
+        
         public AccessLevel AccessLevel { get; set; }
 
         public List<string> Emails { get; set; }
--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,10 +4,11 @@
 //
 //      Major Version
 //      Minor Version 
+
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.8.0")]
-[assembly: AssemblyFileVersion("0.7.8.0")]
+[assembly: AssemblyVersion("0.7.9.0")]
+[assembly: AssemblyFileVersion("0.7.9.0")]
